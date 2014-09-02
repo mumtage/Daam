b6b0b3c1da39f8b2033b1b9697464b9129a18d70
Tollevin Williams <tollevin.williams@spsmail.cuny.edu>
2014-09-02 15:50:54 -0400

    Assignment 01
    R, RStudio, PostgreSQL version, etc

---
+++ Assignment_1.R
@@ -0,0 +1,10 @@
+# 1
+R.version.string;
+  # RStudio Version 0.98.1028 – © 2009-2013 
+# 2
+  # PostgreSQL version 9.3.5
+# 3
+install.packages ("DMwR");
+library(DMwR);
+sales;
+  # There are 401146 observations in the data set
